9bebeb601a2822e1ce26039a5b8c3f9f3122dad8
Nikhil S. Hore <niknsh5@gmail.com>
2021-07-14 14:20:31 +0530

    done

--- site/js/script.js
+++ site/js/script.js
@@ -1,19 +1,18 @@
-// DOM manipulation
+// Handling Events
 
-function sayHello () {
-  this.textContent = "said it!";
-  var name = document.getElementById("name").value;
-  var messege = "<h2>Hello " + name + "!</h2>";
-  // console.log(messege);
-  document.getElementById("content").innerHTML = messege;
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    function sayHello () {
+      console.log(event);
+      this.textContent = "said it!";
+      var name = document.getElementById("name").value;
+      var messege = "<h2>Hello " + name + "!</h2>";
+      document.getElementById("content").innerHTML = messege;
 
-  if (name==="student") {
-    document.querySelector("#title").textContent += " & Lovin' it!";
+      if (name==="student") {
+        document.querySelector("#title").textContent += " & Lovin' it!";
+      }
+    }
+    document.querySelector("button").addEventListener("click", sayHello);
   }
-}
-
-// Unobstrusive event binding
-
-document.querySelector("button").addEventListener("click", sayHello);
-
-// document.querySelector("button").onclick = sayHello;
+);
